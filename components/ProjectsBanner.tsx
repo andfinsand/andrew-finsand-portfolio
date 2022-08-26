91c349afaa176aba94baced94bbb522e97629e1e
Andrew Finsand <andfinsand@gmail.com>
2022-08-26 14:31:54 -0700

    Banner text downsized.

--- components/ProjectsBanner.tsx
+++ components/ProjectsBanner.tsx
@@ -16,7 +16,7 @@ const ProjectsBanner = () => {
       </Head>
       <div className="wideBoxBG flex justify-center bg-dark h-24 -mt-12">
         <div className="wideParent flex justify-center bg-medium h-32 -skew-y-3 -skew-x-3 -mt-4 lg:wideParentBG">
-          <div className="wideBox flex justify-center bg-dark self-center h-24 font-bold text-6xl  skew-y-3 skew-x-3 p-5">
+          <div className="wideBox flex justify-center bg-dark self-center h-24 font-bold text-5xl  skew-y-3 skew-x-3 p-5">
             <div className="self-center text-seagreen font-kanit italic font-bold mr-3">P R O </div>
             <div className="self-center text-white font-kanit italic font-bold"> J E C T S</div>
           </div>
