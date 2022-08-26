24719b71325564d711d250d4459509c753d392a6
Andrew Finsand <andfinsand@gmail.com>
2022-08-26 12:36:55 -0700

    Overflow-x hidden added to global.css to remove white border during responsiveness.

--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -9,12 +9,20 @@ const HomeTitle = () => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Electrolize&family=Kanit:ital,wght@1,600;1,800;1,900&family=Play:wght@400;700&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
       </Head>
-        <div className="text-7xl font-bakbakone font-bold text-white">
-            <p>Full Stack Python Developer</p>
+      <div className="text-7xl font-bakbakone font-bold text-white">
+          <p>Full Stack Python Developer</p>
+      </div>
+      <div className="text-center text-4xl font-bakbakone text-seagreen sm:text-left">
+          <p>Andrew Finsand</p>
+      </div>
+      <div className="my-36">
+        <div className="text-2xl text-white font-semibold font-electrolize">
+          <p>Welcome to my portfolio</p>
         </div>
-        <div className="text-center text-4xl font-bakbakone text-seagreen sm:text-left">
-            <p>Andrew Finsand</p>
+        <div className="flex justify-center my-5 mx-9 sm:justify-start">
+        <a data-scroll="projects" href="#projects" className="btn btn-secondary text-dark font-bold bg-seagreen rounded-full border-seagreen px-6 hover:bg-dark hover:text-seagreen hover:border-seagreen">Jump to My Work</a>
         </div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 
 import Navbar from '../components/Navbar'
 import HomeTitle from '../components/HomeTitle'
-import HomeSubTitle from '../components/HomeSubTitle'
 import About from '../components/About'
 import ProjectCards from '../components/ProjectCards'
 import Skills from '../components/Skills'
@@ -13,9 +12,8 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col" id="home">
       <Navbar />
-      <div className="bg-bgImage bg-cover bg-fixed bg-center">
+      <div className="bg-bgImage bg-cover bg-fixed bg-center w-full">
         <HomeTitle />
-        <HomeSubTitle />
         <About />
       </div>
       <ProjectCards />
