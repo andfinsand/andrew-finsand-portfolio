76c2a35d76adbd2e5d6d29caef47f2baa8ac1603
Andrew Finsand <andfinsand@gmail.com>
2023-01-04 10:06:08 -0800

    Update landing fonts size. Revise About Me.

--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -9,7 +9,7 @@ const HomeTitle = () => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Electrolize&family=Kanit:ital,wght@1,600;1,800;1,900&family=Play:wght@400;700&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
       </Head>
-      <div className="text-7xl font-bakbakone font-bold text-white">
+      <div className="text-6xl font-bakbakone font-semi text-white">
           <p>Full Stack Python Developer</p>
       </div>
       <div className="text-center text-4xl font-bakbakone text-seagreen m-5">
